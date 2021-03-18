d8e144b3c56a17f77bc142de04bad2f39c552f07
Luco <renatoluco@gmail.com>
2021-03-18 16:31:40 -0300

    Caja de sugerencias en el buscadr por paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,33 +5,43 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  styleUrls: ['./por-pais.component.css'],
 })
 export class PorPaisComponent {
-
   termino: string = '';
   hayError: boolean = false;
   tipo: string = 'Buscar por pais...';
-  paises: Country[] = []
+  paises: Country[] = [];
+  paisesSugerencia: Country[] = [];
 
-  constructor(private paisService: PaisService) { }
+  constructor(private paisService: PaisService) {}
 
-  buscar_pais(termino: string){
-    this.termino = termino
+  buscar_pais(termino: string) {
+    this.paisesSugerencia = [];
+    this.termino = termino;
     this.hayError = false;
     console.log(this.termino);
 
-    this.paisService.buscarPais(this.termino)
-        .subscribe(paises => {
-          //console.log(paises);
-          this.paises = paises;
-        }, err => {
-          this.hayError = true;
-        });
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        //console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+      }
+    );
   }
 
-  sugerencias(termino: string){
+  //Sugerencias
+  sugerencias(termino: string) {
     this.hayError = false;
-
+    this.paisService.buscarPais(termino)
+        .subscribe(paises =>{
+      this.paisesSugerencia = paises.splice(0,5);
+    },
+        err => {
+          this.paisesSugerencia = [];
+        });
   }
 }
